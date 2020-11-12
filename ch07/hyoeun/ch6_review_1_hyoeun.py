2f07fe3dcafe17a815275e9623a9df1dac977c36
hyo-eun-kim <heun7410@naver.com>
2020-11-13 03:44:51 +0900

    ch6_review_hyoeun

--- ch07/hyoeun/ch6_review_1_hyoeun.py
+++ ch07/hyoeun/ch6_review_1_hyoeun.py
@@ -6,10 +6,16 @@ def shortest_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s
     else:
-        
+        index = 1
+        # find palindrome
+        for i in range(len(s), 0, -1):
+            if s[:i] == s[:i][::-1]:
+                index = i
+                break
+        s = s[index:][::-1]+s
+        return s
 
 
 if __name__ == "__main__":
-    s = "aacecaaa"
-    print(shortest_palindrome("aaa"))
-    print(shortest_palindrome(s))
+    print(shortest_palindrome("aacecaaa"))
+    print(shortest_palindrome("abcd"))
