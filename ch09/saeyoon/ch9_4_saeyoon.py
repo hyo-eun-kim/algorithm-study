8960061ece4ca739dfcdfed1270112ea96f0e574
leesaeyoon <ericalee97@naver.com>
2020-11-27 20:37:27 +0900

    ch9_saeyoon

--- ch09/saeyoon/ch9_4_saeyoon.py
+++ ch09/saeyoon/ch9_4_saeyoon.py
@@ -22,10 +22,10 @@ class MyStack:
             self.q.append(self.q.popleft())
 
     def pop(self) -> int:
-        return self.q.pop()
+        return self.q.popleft()
 
     def top(self) -> int:
-        return self.q[-1]
+        return self.q[0]
 
     def empty(self) -> bool:
         return not self.q
