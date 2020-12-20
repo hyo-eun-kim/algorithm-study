3afc3d34907cb5402f3cc23752786e39f2b00e02
hyo-eun-kim <heun7410@naver.com>
2020-12-21 02:28:43 +0900

    ch9_review_hyoeun

--- ch09/hyoeun/ch09_2_hyoeun.py
+++ ch09/hyoeun/ch09_2_hyoeun.py
@@ -9,11 +9,23 @@ Input: s = "bcabc"      Output: "abc"
 Input: s = "cbacdcbc"   Output: "acdb"
 '''
 
-
 def removeDuplicateLetters(s: str) -> str:
-# 아직 해결 X
+    d = {char: index for index, char in enumerate(s)}
+    final_s = []
+
+    for index, char in enumerate(s):
+        if char not in final_s:
+            # final_s가 비어있지 X AND
+            # 마지막의 요소보다 char의 우선순위가 높고 AND
+            # 마지막의 요소는 뒤에서 한 번 더 나타난다는 보장 있는 경우
+            # -> 마지막 요소 pop
+            while final_s and final_s[-1] > char and d[final_s[-1]] > index:
+                final_s.pop()
+            final_s.append(char)
+    return "".join(final_s)
 
 
 if __name__ == "__main__":
-    print(removeDuplicateLetters("bcabc"))
-    print(removeDuplicateLetters("cbacdcbc"))
+
+    removeDuplicateLetters("bcabc")     # abc
+    removeDuplicateLetters("cbacdcbc")  # acdb
